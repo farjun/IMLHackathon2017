ab23ff9b32ea34df4098e0b8bd94beb527bdbcb9
Ben Faingold <benfaingold@gmail.com>
2017-06-22 16:36:32 +0300

    Add src files

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -1,20 +1,20 @@
 import numpy as np
 import pandas as pd
 import re
-#preproccessibng
+# preproccessibng
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-#algorithems
+# algorithems
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 
-def readFiles():
-    haaretzHeadlings = pd.read_csv("./Training set/Headlines/haaretz.csv",names = ['Headers'])
-    israelHayomHeadlines = pd.read_csv("./Training set/Headlines/israelhayom.csv",names=['Headers'])
-    return haaretzHeadlings, israelHayomHeadlines
 
+def readFiles():
+    haaretzHeadlings = pd.read_csv("./Training set/Headlines/haaretz.csv", names=['Headers'])
+    israelHayomHeadlines = pd.read_csv("./Training set/Headlines/israelhayom.csv", names=['Headers'])
+    return haaretzHeadlings, israelHayomHeadlines
 
 
 def normelizeText(s):
@@ -30,21 +30,23 @@ def main():
     print(haaretzHeadlings)
     print(israelHayomHeadlines)
 
-
-    #init vectorizer
-    vectorizer = CountVectorizer()
-    Vheaders1 = vectorizer.fit_transform(haaretzHeadlings['Headers'])
-    Vheaders2 = vectorizer.fit_transform(israelHayomHeadlines['Headers'])
+    # init vectorizer
+    vectorizer1 = CountVectorizer()
+    Vheaders1 = vectorizer1.fit_transform(haaretzHeadlings['Headers'])
+    vectorizer2 = CountVectorizer()
+    Vheaders2 = vectorizer2.fit_transform(israelHayomHeadlines['Headers'])
     print('---------------')
     print(Vheaders2)
     print('---------------')
     print(Vheaders2.shape)
     print('---------------')
     print(Vheaders1)
+    print('---------------')
+    print(vectorizer1.get_feature_names())
+    print(vectorizer2.get_feature_names())
+
 
 
 
 if __name__ == '__main__':
     main()
-
-
