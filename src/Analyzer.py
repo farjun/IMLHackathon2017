f157ebfdc40dee9a644e9f59dca1d1e9350a3e73
omer <omerbh1@gmail.com>
2017-06-22 16:10:44 +0300

    Course material

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 #preproccessibng
 
 from sklearn.model_selection import train_test_split
@@ -10,10 +11,21 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 
 def readFiles():
-    haaretzHeadlings = pd.read_csv("./Training set/Headlines/haaretz.csv")
-    israelHayomHeadlines = pd.read_csv("./Training set/Headlines/israelhayom.csv")
+    haaretzHeadlings = pd.read_csv("./Training set/Headlines/haaretz.csv",names = ['Headres'])
+    israelHayomHeadlines = pd.read_csv("./Training set/Headlines/israelhayom.csv",names=['Headres'])
     return haaretzHeadlings, israelHayomHeadlines
 
 haaretzHeadlings ,israelHayomHeadlines = readFiles()
 
+
+def normalize_text(s):
+    s = s.lower()
+    s = re.sub('\s\W', ' ', s)
+    s = re.sub('\W\s', ' ', s)
+    s = re.sub('\s+', ' ', s)
+    return s
+
+print(haaretzHeadlings)
+haaretzHeadlings = [normalize_text(s) for s in haaretzHeadlings]
+print("sdkjhfs",haaretzHeadlings)
 
