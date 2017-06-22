ce41f3d3ec72bb08277b68b02693a6fd02af6642
omer <omerbh1@gmail.com>
2017-06-22 15:54:03 +0300

    Course material

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -1,7 +1,19 @@
 import numpy as np
 import pandas as pd
-from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+#preproccessibng
 
-headline = pd.read_csv("")
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+#algorithems
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+
+def readFiles():
+    haaretzHeadlings = pd.read_csv("./Training set/Headlines/haaretz.csv")
+    israelHayomHeadlines = pd.read_csv("./Training set/Headlines/israelhayom.csv")
+    return haaretzHeadlings, israelHayomHeadlines
+
+haaretzHeadlings ,israelHayomHeadlines = readFiles()
+
+
