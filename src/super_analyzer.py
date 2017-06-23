acbfbe34f7500f2ba6c8fd03fe20eb50ae873dfa
Ben Faingold <benfaingold@gmail.com>
2017-06-23 07:58:42 +0300

    Holy shit

--- src/super_analyzer.py
+++ src/super_analyzer.py
@@ -9,76 +9,74 @@ from pandas import DataFrame
 from nltk import pos_tag, word_tokenize
 from collections import OrderedDict
 
-
-def readFiles():
-    haaretz_headlines = pd.read_csv("../Training set/Headlines/haaretz.csv", names=['Headers'])
-    israel_hayom_headlines = pd.read_csv("../Training set/Headlines/israelhayom.csv", names=['Headers'])
-    return haaretz_headlines, israel_hayom_headlines
-
-
-haaretzHeadlines, israelHayomHeadlines = readFiles()
-haaretzHeadlines['label'] = 0
-israelHayomHeadlines['label'] = 1
-
-base_df = pd.concat([haaretzHeadlines, israelHayomHeadlines])
-
-all_headlines = []
+all_headlines = None
 lengths = []
 avg_word_len = []
 dot = []
 tags = OrderedDict()
 
-i = 0
-for headline in haaretzHeadlines['Headers']:
-    lengths.append(len(headline))
-    word_lengths = [len(w) for w in headline]
-    avg_word_len.append(sum(word_lengths) / len(word_lengths))
-    dot.append(headline.count('.'))
-    pt = pos_tag(word_tokenize(headline))
-    for t in pt:
-        if t[1] in tags:
-            tags[t[1]][i] += 1
-        else:
-            tags[t[1]] = [1] + [0] * (len(haaretzHeadlines) + len(israelHayomHeadlines) - 1)
-    all_headlines.append(headline)
-    i += 1
 
-i = 0
-for headline in israelHayomHeadlines['Headers']:
-    lengths.append(len(headline))
-    word_lengths = [len(w) for w in headline]
-    avg_word_len.append(sum(word_lengths) / len(word_lengths))
-    dot.append(headline.count('.'))
-    pt = pos_tag(word_tokenize(headline))
-    for t in pt:
-        if t[1] in tags:
-            tags[t[1]][i] += 1
-        else:
-            tags[t[1]] = [1] + [0] * (len(haaretzHeadlines) + len(israelHayomHeadlines) - 1)
-    all_headlines.append(headline)
-    i += 1
+def read_files():
+    return pd.read_csv("../Training set/Headlines/haaretz.csv", names=['headlines']), \
+           pd.read_csv("../Training set/Headlines/israelhayom.csv", names=['headlines'])
 
-# pull the data into vectors
-vectorizer = CountVectorizer(ngram_range=(1, 2))
-x = vectorizer.fit_transform(all_headlines)
-print(x.shape)
-df = DataFrame(x.A, columns=vectorizer.get_feature_names())
-df['lengths'] = lengths
-df['avg_word_len'] = avg_word_len
-df['dot'] = dot
-for (k, v) in tags.items():
-    df[k] = v
 
-# split into train and test sets
-x_train, x_test, y_train, y_test = train_test_split(sparse.csr_matrix(df.values),
-                                                    np.append(haaretzHeadlines['label'],
-                                                              israelHayomHeadlines['label']),
-                                                    test_size=0.2, random_state=42)
+def process(headlines):
+    i = 0
 
-vc = MLPClassifier()
-vc.fit(x_train, y_train)
+    for h in headlines:
+        lengths.append(len(h))
+        word_lengths = [len(w) for w in h]
+        avg_word_len.append(sum(word_lengths) / len(word_lengths))
+        dot.append(h.count('.'))
+        pt = pos_tag(word_tokenize(h))
 
-# Print the accuracy
+        for tok_tag in pt:
+            tag = tok_tag[1]
 
-print(f"Train Accuracy: {vc.score(x_train, y_train)}")
-print(f"Test Accuracy: {vc.score(x_test, y_test)}")
+            if tag in tags:
+                tags[tag][i] += 1
+            else:
+                tags[tag] = [1] + [0] * (len(all_headlines) - 1)
+
+        i += 1
+
+
+if __name__ == '__main__':
+    print('Fetching data...')
+    haaretz_headlines, israel_hayom_headlines = read_files()
+    haaretz_headlines['label'] = 0
+    israel_hayom_headlines['label'] = 1
+    all_headlines = np.concatenate((haaretz_headlines['headlines'], israel_hayom_headlines['headlines']))
+
+    print('Vectorizing data...')
+    vectorizer = CountVectorizer(ngram_range=(1, 2))
+    x = vectorizer.fit_transform(all_headlines)
+    df = DataFrame(x.A, columns=vectorizer.get_feature_names())
+
+    print('Processing Haaretz headlines...')
+    process(haaretz_headlines['headlines'])
+    print('Processing Israel Hayom headlines...')
+    process(israel_hayom_headlines['headlines'])
+
+    print('Aggregating features...')
+    df['lengths'] = lengths
+    df['avg_word_len'] = avg_word_len
+    df['dot'] = dot
+
+    for (k, v) in tags.items():
+        df[k] = v
+
+    # Split into train and test sets
+    x_train, x_test, y_train, y_test = train_test_split(sparse.csr_matrix(df.values),
+                                                        np.append(haaretz_headlines['label'],
+                                                                  israel_hayom_headlines['label']),
+                                                        test_size=0.2, random_state=42)
+
+    print('Training model...')
+    mlp = MLPClassifier()
+    mlp.fit(x_train, y_train)
+
+    # Print accuracy results
+    print(f"Train Accuracy: {mlp.score(x_train, y_train)}")
+    print(f"Test Accuracy: {mlp.score(x_test, y_test)}")
